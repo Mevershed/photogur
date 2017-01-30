09ee4f96706b76fb3cd65a0cd41159342cf34834
Mevershed <matthew@evershed.ca>
2017-01-30 14:06:19 -0500

    config root as pictures#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
-  get 'pictures/:id' => 'pictures#show', as: 'picture' 
+
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new
+  '
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
